6817b296c036531f1e35b390f5e329de814524cf
Laserology <admin@laserology.net>
2026-05-20 22:21:24 -0700

    Remove clanker nonsense

--- custom_components/govee-ble-lights/const.py
+++ custom_components/govee-ble-lights/const.py
@@ -15,9 +15,3 @@ DOMAIN = "govee-ble-lights"
 # Configuration key used to identify BLE as the configuration type
 # This is used during the config flow to distinguish between different integration types
 CONF_TYPE_BLE = "BLE"
-
-# TODO: Add any additional constants here as the integration grows
-# Examples:
-# - DOMAIN = Unique identifier for this integration
-# - CONF_* = Configuration entry keys
-# - BLE_* = BLE-specific timing or protocol constants
